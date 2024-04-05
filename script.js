5a94e7a23ee2e52b81ce45fc67e8e8b4cb73f19e
yarhamalfatih <falillahyarham@gmail.com>
2024-04-06 02:23:36 +0700

    add command

--- script.js
+++ script.js
@@ -13,13 +13,19 @@ function executeCommand(command) {
     let output = '';
     switch (command) {
         case 'help':
-            output = "Perintah tersedia: <br> - help: Menampilkan list perintah <br> - about: Tentang saya <br> - clear: Membersihkan terminal";
+            output = "Perintah tersedia: <br> 'about': Menampilkan tentang saya <br> 'skills': Menampilkan skill saya <br> 'instagram': Mengunjungi profil Instagram saya <br> 'clear': Membersihkan terminal";
             break;
         case 'about':
             output = "<div style='display: flex; align-items: center;'><img src='assets/yarham.jpg' alt='Your Image' style='max-width: 50%; margin-top: 20px;'><p style='margin-left: 45px;'>I'm a Software Engineer</p></div>";
             break;
+        case 'skills':
+            output = "Skill saya:<br>- HTML<br>- CSS<br>- C#<br>- Java<br>- JavaScript<br>- Python<br>- PHP";
+            break;
+        case 'instagram':
+            window.open('https://www.instagram.com/yarhamalfatih/', '_blank');
+            output = "Mengarahkan Anda ke profil Instagram saya...";
+            break;
         case 'clear':
-            // Hapus semua output kecuali teks awal dan teks di pre-terminal-output
             const terminalOutputs = document.querySelectorAll('.terminal-output');
             terminalOutputs.forEach(output => {
                 if (!output.classList.contains('pre-terminal-output') && output.innerHTML !== '[root@yarhamalfatih:~]# start https://yarhamalfatih.github.io' && output.innerHTML !== '[root@yarhamalfatih:~]# Ketik \'help\' untuk melihat list perintah') {
@@ -31,7 +37,7 @@ function executeCommand(command) {
             output = `Perintah ${command} tidak tersedia`;
             break;
     }
-    // Tambahkan output hanya jika bukan perintah 'clear'
+
     if (command !== 'clear') {
         const outputElement = document.createElement('p');
         outputElement.classList.add('terminal-output');
